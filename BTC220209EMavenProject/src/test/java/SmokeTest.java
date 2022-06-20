2378d24e016ba7c6a33e2826a88bd8d476afcc69
Zinat Rehana <zinatrehana@zinats-air.lan>
2022-06-20 19:19:29 -0400

    added

---
+++ BTC220209EMavenProject/src/test/java/SmokeTest.java
@@ -0,0 +1,63 @@
+import org.junit.Test;
+
+public class SmokeTest {
+	
+	
+	@Test
+	
+	public void sampleTest() {
+		
+		
+		System.out.println("This is a Maven Test.............");
+		
+	}
+	
+	
+	@Test
+	public void print_By_Word() {
+		
+		String g = "how are you";
+		char ch[] = g.toCharArray();
+		// print by word
+		String k[] = g.split(" ");
+		//int l = k.length;
+
+		for (int j = k.length - 1; j >= 0; j--) {
+			System.out.println(k[j]);
+		}
+	
+	
+			StringBuffer sb = new StringBuffer();
+			
+			String m = "How are you";
+			String word [] = g.split(" ");
+			for (String w:word) { 
+				
+				for( int i = word.length-1; i>= 0; i--) {
+				
+				sb.append(" ").toString().trim();
+			
+				
+				
+				
+			
+			}
+			
+			
+	}
+			
+			
+			
+			char ch[] = g.toCharArray();
+			// print by word
+			String k[] = g.split(" ");
+			//int l = k.length;
+
+			for (int j = k.length - 1; j >= 0; j--) {
+				System.out.println(k[j]);
+			}
+		
+		
+	}
+
+}
